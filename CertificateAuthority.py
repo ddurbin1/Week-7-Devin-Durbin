17864c78950d21f9be9260e81fb75b7753c56751
ddurbin1 <ddurbin1@live.maryville.edu>
2020-02-29 21:36:08 -0600

    Update to all files
    Major update on all files and all connect properly, still to clean up.

--- CertificateAuthority.py
+++ CertificateAuthority.py
@@ -1,9 +1,44 @@
 import socket
-from cryptography.fernet import Fernet
-key = Fernet.generate_key()
+from datetime import datetime
 
-CA = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = 9501
 
-CA.bind((socket.gethostname(), 9501))
-CA.listen(5)
+def main():
+    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connection.bind((socket.gethostname(), port))
 
+    connection.listen()
+    certificates = {}
+
+    run_server = True
+    while run_server:
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        
+        response = ""
+        session, addr = connection.accept()
+        received_data = session.recv(1024).decode().split(',')
+        action = received_data[0]
+        host = received_data[1]
+        public_key = received_data[2]
+        print("Validation date:", current_time)
+        print(action + "," + socket.gethostname() + "," + public_key)
+
+        if action == 'Register':
+            certificates[socket.gethostname()] = public_key
+            response = "200"
+
+        elif action == 'Validate':
+            if socket.gethostname() in certificates:
+                response = certificates[socket.gethostname()]
+            else:
+                response = None
+
+        else:
+            response = ""
+
+        if response != "":
+            session.send(response.encode())
+
+        session.close()
+main()
--- ServerSocket.py
+++ ServerSocket.py
@@ -1,15 +1,73 @@
 import socket
 
-serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = 9500
+servername = "Server 1"
+public_key = +1
+private_key = -1
+confirmation = '12037'
 
-serv.bind((socket.gethostname(), 9500))
-serv.listen(5)
+def server():
+    cert_auth_connection = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
+    cert_auth_connection.connect((socket.gethostname(), 9501))
+    cert_auth_certificate = "Register" + "," + servername + "," + str(public_key)
+    cert_auth_connection.send(cert_auth_certificate.encode())
+    cert_auth_respond = cert_auth_connection.recv(1024).decode()
+    cert_auth_connection.close()
+    return (cert_auth_respond == "200")
 
-while True:
-    conn, addr = serv.accept()
-    msg = conn.recv(1024)
-    print(msg.decode("utf-8"))
-    if (msg == b"Hello"):
-        conn.send(bytes("Hi", "utf-8"))
+def decrypt(text):
+    decrypted_copy = ''
+    for char in text:
+        decrypted_copy += chr(ord(char) + private_key)
+    return decrypted_copy
+
+def encrypt(text):
+    encrypted_copy = ''
+    for char in text:
+        encrypted_copy += chr(ord(char) + public_key)
+    return encrypted_copy
+
+def main():
+    if server():
+        print("Register Successful")
+
+        while True:
+            connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            connection.bind((socket.gethostname(), port))
+            connection.listen()
+            
+            print(socket.gethostname() + ": port " + str(port) + " active")
+
+            session, addr = connection.accept()
+
+            run_server = True
+            while run_server:
+                    receive_data = session.recv(1024).decode()
+                    decrypted_copy = decrypt(receive_data)
+                    print(decrypted_copy)
+                    
+                    if decrypted_copy == 'Hi':
+                        output_data = "Hello"
+                        print("Response: \'" + output_data +"\'")
+                        session.send(output_data.encode())
+                        
+                    elif decrypted_copy == "Bye":
+                        output_data = "Good Bye"
+                        session.send(output_data.encode())
+                        break
+                    
+                    else:
+                        print("Text received: '" + receive_data + "'")
+                        print("Client request: '" + decrypted_copy + "'")
+                        if decrypted_copy == confirmation:
+                            output_data = encrypt(confirmation + 'validation')
+                        else:
+                            print("Message handled")
+                            output_data = encrypt("Confirmed – Message handled")
+
+                        print("Sending response: \'" + output_data +"\'")
+                        session.send(output_data.encode())
     else:
-        conn.send(bytes("Goodbye", "utf-8"))
+        print("Connection requirements not met.")
+        exit
+main()
